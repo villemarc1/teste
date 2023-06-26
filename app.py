d94c70444c216136007d57f41bef951c87b57e10
villemarc1 <47375397+villemarc1@users.noreply.github.com>
2023-06-26 06:59:07 -0300

    Update app.py   adicionado @app.route('/read_excel')

--- app.py
+++ app.py
@@ -1,8 +1,11 @@
 from flask import Flask, jsonify, request, render_template
 import pandas as pd
+import os
 
 app = Flask(__name__)
 
+
+UPLOAD_FOLDER = 'uploads'    #nova lina
 stores = [{
     'name': 'My Store',
     'items': [{'name':'my item', 'price': 15.99 }]
@@ -18,6 +21,14 @@ def create_excel():
     df = pd.DataFrame(data)
     df.to_excel('excel_output.xlsx', index=False)
     return 'Excel file created!'
+
+@app.route('/read_excel')
+def read_excel():
+    filename = 'excel_output.xlsx'  # Nome do arquivo a ser exibido
+    #filepath = os.path.join(app.config['UPLOAD_FOLDER'], filename)
+    df = pd.read_excel(filename)
+    return df.to_html()
+
 
 if __name__ == '__main__':
     app.run()
